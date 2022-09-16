db0c905e8e9e7ac8af3df33892d52c3268840b00
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-09-16 12:55:21 -0400

    Firebase initialized

--- no_finish_line_app/lib/main.dart
+++ no_finish_line_app/lib/main.dart
@@ -9,6 +9,7 @@ import '../pages/newWorkout.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
